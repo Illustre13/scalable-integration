f8b4f29e7d6cc8635a62247763dd6946fc42b123
Illustre <ndahayosibertin17@gmail.com>
2026-02-05 03:52:52 +0200

    tryout

--- java-producers/src/main/java/com/integration/java_producers/publisher/EventPublisher.java
+++ java-producers/src/main/java/com/integration/java_producers/publisher/EventPublisher.java
@@ -3,6 +3,7 @@ package com.integration.java_producers.publisher;
 import com.integration.java_producers.config.RabbitMQConfig;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Component
 public class EventPublisher {
@@ -25,4 +26,10 @@ public class EventPublisher {
         //still passing the string here ...................
         // both ways stillll works ....
     }
+
+    // @GetMapping("/send-test")
+    // public String sendTest() {
+    //     rabbitTemplate.convertAndSend("New Queue Event", "Hello RabbitMQ!");
+    //     return "Message sent!";
+    // }
 }
