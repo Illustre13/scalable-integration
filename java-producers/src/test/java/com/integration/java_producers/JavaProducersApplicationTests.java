e343fa839eb7533662e683960e33b8d16de1b049
Illustre <ndahayosibertin17@gmail.com>
2026-02-05 02:52:23 +0200

    fixed the issue with the test cases to now passing error free,,,,

--- java-producers/src/test/java/com/integration/java_producers/JavaProducersApplicationTests.java
+++ java-producers/src/test/java/com/integration/java_producers/JavaProducersApplicationTests.java
@@ -1,13 +1,22 @@
 package com.integration.java_producers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.integration.java_producers.client.InventoryClient;
 
 @SpringBootTest
 class JavaProducersApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockitoBean
+    private InventoryClient inventoryClient;
 
+    @MockitoBean
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    void contextLoads() {}
 }
+
